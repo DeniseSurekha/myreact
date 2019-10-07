80b4c3e6f27b0a12512f293e7974bdd9c64ce037
DeniseSurekha <denisesurekha@gmail.com>
2019-10-07 23:27:44 +0530

    Updates

---
+++ precache-manifest.07c5776265b00b9231faaf71635d29ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378e5a6bb3fdae07dec292e48e8d48f7",
+    "url": "/reactjs-login-nd-register/index.html"
+  },
+  {
+    "revision": "1c562ba9e0b3712bce8d",
+    "url": "/reactjs-login-nd-register/static/css/main.0989f10c.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/reactjs-login-nd-register/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "1c562ba9e0b3712bce8d",
+    "url": "/reactjs-login-nd-register/static/js/main.a32cd13e.chunk.js"
+  },
+  {
+    "revision": "9e6bbded9e047e22c281",
+    "url": "/reactjs-login-nd-register/static/js/runtime-main.032805af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactjs-login-nd-register/static/media/logo.25bf045c.svg"
+  }
+]);
